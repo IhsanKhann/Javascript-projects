30b8390a2c16455669e9e81dfa3a53f6704b6350
Ihsan <mortalihsan@gmail.com>
2025-07-07 13:07:38 +0500

    Test commit

--- CurrencyConverter/app.js
+++ CurrencyConverter/app.js
@@ -82,4 +82,4 @@ ExchangeBtn.addEventListener("click", ExchangeRates);
 // how to add options to the select using javaScript
 // eventSelectors.
 
-console.log("Currency Converter Loaded");
+console.log("Currency Converter Loaded again");
